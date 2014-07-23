c646e87a29b93c8bbc9b6002cbe4b46852efacaa
Matt Highland <ichibangaijin77@gmail.com>
2014-07-23 12:26:26 -0700

    updated some projects

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$(".portfolio-box").click(function(){
 		$(".portfolio-box").css("display", "none");
 		$(".page-title").text("Some of my recent projects...");
-		$(".page-description").text("I believe the web is mankind's greatest creation.  Never before has the world been so connected, and now that I am becoming a developer, I feel like I am helping to shape the future...")
+		$(".page-description").text("I believe the web is mankind's greatest creation.  Never before has the world been so connected, and now that I am becoming a developer, I feel like I am part of this legacy.");
 		$(".hidden-slide-show").show();
 				
 	
