a723333bc31d7f1a489ba8493a99d79c20e04ee5
Matt Highland <ichibangaijin77@gmail.com>
2014-07-28 23:54:46 -0700

    changed colors and adjusted margins on hidden box

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 		$(".page-title").text("Some of my recent projects...");
 		$(".page-description").text("I believe the web is mankind's greatest creation.  Never before has the world been so connected, and now that I am becoming a developer, I feel like I am part of this legacy.");
 		$(".hidden-slide-show").show();
+		alert("So sorry, this page is under construction.  Please email for links to the projects. You will find the home button and email link at the top of the page.  Thanks!  Matt.");
 				
 	
 
